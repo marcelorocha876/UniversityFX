9e1d6ad98fdf368821b4a4df19947668923dde40
Marcelo Rocha <marcelorocha876.mr@gmail.com>
2021-10-11 22:48:06 +0100

    Initial commit

---
+++ src/university/Student.java
@@ -0,0 +1,26 @@
+package university;
+
+import algs4.SeparateChainingHashST;
+
+import java.io.Serializable;
+
+
+public class Student extends Person {
+
+  public Long number;
+
+  public Student(String name, String address, Date birth, String email, Date registration, Long number) {
+    super(name, address, birth, email, registration);
+    this.number = number;
+  }
+
+  public Long getNumber() {
+    return number;
+  }
+
+  public void setNumber(Long number) {
+    this.number = number;
+  }
+
+
+}
